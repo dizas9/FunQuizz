9ac90fdf18066ac7797c0518abbda10b895c74b3
dizas9 <sazid.ice07@gmail.com>
2024-05-11 21:52:36 +0600

    Test on Render

--- src/API.jsx
+++ src/API.jsx
@@ -1,6 +1,6 @@
 export const PROD_URL = "";
 export const PRE_URL = "";
-export const DEV_URL = "https://fun-quizz-server.vercel.app";
+export const DEV_URL = "https://funquizz.onrender.com";
 //https://fun-quizz-server.vercel.app
 //https://funquizz.onrender.com
 //http://localhost:5000
