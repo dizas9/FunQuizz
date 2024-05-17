48748c262963246bebbd86211dd6d838cc812323
dizas9 <sazid.ice07@gmail.com>
2024-05-17 14:13:01 +0600

    MAY Week 3 update

--- server/Routes/QuizContest.js
+++ server/Routes/QuizContest.js
@@ -116,7 +116,7 @@ router.get("/contest_lists", async (req, res) => {
     const contests = await db.listCollections({}, { nameOnly: true }).toArray();
     const targetCollection = contests
       .map((collection) => collection.name)
-      .filter((name) => name.startsWith("MAY_W2"));
+      .filter((name) => name.startsWith("MAY_W3"));
 
     //find Contest Info
     const contestsInfo = [];
